be8349c48a15c204edd9583313cac24bf4fc4b39
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2016-12-19 20:24:21 +0900

    initial commit

---
+++ docs/app.jsx
@@ -0,0 +1,139 @@
+const $ = require('jquery');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const AWS = require('aws-sdk');
+const UUID = require('uuid');
+
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'ap-northeast-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'ap-northeast-1:06a88243-bfb8-4122-8fc8-879c85b4be72'
+});
+
+const KumaImage = React.createClass({
+  render: function () {
+    let image = '';
+    if (this.props.imageUrl) {
+        image = <img src={this.props.imageUrl}/>
+    }
+    return (
+      <div>{image}</div>
+    );
+  }
+});
+
+const App = React.createClass({
+  getInitialState: function() {
+    return {
+      file: null,
+      filename: '',
+      imageUrl: null,
+      progress_state: 'initial'
+    };
+  },
+
+  handleSelectFile: function(event) {
+    const file = event.target.files[0];
+    if (file) {
+      this.setState({
+        file: file,
+        filename: UUID.v4() + '-' + file.name,
+        progress_state: 'fileselected'
+      });
+    } else {
+      this.setState({
+        file: null,
+        filename: null,
+        progress_state: 'initial'
+      });
+    }
+  },
+
+  uploadClick: function (event) {
+    this.uploadFile();
+  },
+
+  uploadFile: function () {
+    this.setState({progress_state: 'file_uploading'})
+    const s3 = new AWS.S3();
+    const object = {
+      Bucket: 'naritaijinsei',
+      Key: 'input/' + this.state.filename,
+      ContentType: this.state.file.type,
+      Body: this.state.file
+    };
+    const self = this;
+    s3.putObject(object, function(err, data) {
+      if (err) {
+        console.log("Error :" + err);
+      } else {
+        console.log("Successfully uploaded");
+      }
+      self.setState({progress_state: 'file_uploaded'});
+    });
+  },
+
+  narimasuClick: function (event) {
+    this.setState({progress_state: 'api_requesting'});
+    const config = "neko_config.json";
+    const source_image = this.state.filename;
+    const api_endpoint = "https://2vddqdwgcl.execute-api.ap-northeast-1.amazonaws.com/beta"
+    const param = {
+      config: config,
+      source_image: source_image
+    };
+    //const url = api_endpoint + '?' + 'config=' + config + '&source_image=' + source_image;
+    //console.log(url);
+    const self = this;
+    $.getJSON(api_endpoint, param).then(
+      function (data) {
+        console.log("image_url:" + data["image_url"]);
+        self.setState({imageUrl: data["image_url"]});
+      },
+      function (data) {
+        console.log('Error: ' + data);
+      }
+    ).done(
+      function() {
+        self.setState({progress_state: 'api_requested'})
+      }
+    );
+
+  },
+
+  canUpload: function () {
+    if (this.state.progress_state == 'file_selected') {
+      return true;
+    }
+    return false;
+  },
+
+  canBeKuma: function () {
+    if (this.state.progress_state == 'file_uploaded') {
+      return true;
+    }
+    return false;
+  },
+
+  render: function() {
+    return (
+      <div>
+        <input type="file" ref="file" onChange={this.handleSelectFile} />
+        <button onClick={this.uploadClick}>
+          ファイルをアップロード
+        </button>
+        <button onClick={this.narimasuClick}>
+          猫になる！
+        </button>
+        <KumaImage
+          imageUrl={this.state.imageUrl}
+        />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
---
+++ lambda/main.py
@@ -0,0 +1,77 @@
+# coding: UTF-8
+
+from __future__ import print_function
+
+import cv2
+import numpy as np
+import boto3
+import uuid
+import json
+
+s3 = boto3.client('s3')
+
+def get_config_from_s3(bucket, folder, file):
+    tmp_config_filename = '/tmp/' + uuid.uuid4().hex + file
+    key = folder + file
+    s3.download_file(Bucket = bucket, Key = key, Filename = tmp_config_filename)
+    with open(tmp_config_filename, 'r') as f:
+        config = json.load(f)
+    return config
+
+def get_image_from_s3(bucket, folder, file):
+    tmp_image_filename = '/tmp/' + uuid.uuid4().hex + file
+    key = folder + file
+    s3.download_file(Bucket = bucket, Key = key, Filename = tmp_image_filename)
+    return cv2.imread(tmp_image_filename)
+
+def face_detector(image):
+    gray_image  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    return faceCascade.detectMultiScale(gray_image, 1.1, 3)
+
+def trim_image(image, rect): # rect = [x, y, width, height]
+    x, y, width, height = rect
+    return image[y:y + height, x:x + width]
+
+def put_image_to_s3(image, bucket, folder, file):
+    tmp_image_filename = '/tmp/' + uuid.uuid4().hex + file
+    cv2.imwrite(tmp_image_filename, image)
+    key = folder + file
+    s3.upload_file(Filename = tmp_image_filename, Bucket = bucket, Key = key)
+    return True
+
+
+########################################################
+def lambda_handler(event, context):
+    #
+    S3_BUCKET           = 'naritaijinsei'
+    S3_OUTPUT_FOLDER    = 'output/'
+    S3_INPUT_FOLDER     = 'input/'
+    S3_UTIL_FOLDER      = 'util/'
+
+    config  = get_config_from_s3(bucket = S3_BUCKET, folder = S3_UTIL_FOLDER, file = event["config"])
+
+    # get images
+    source_image    = get_image_from_s3(bucket = S3_BUCKET, folder = S3_INPUT_FOLDER,file = event["source_image"])
+    base_image      = get_image_from_s3(bucket = S3_BUCKET, folder = S3_UTIL_FOLDER, file = config["base_image"])
+    mask_image      = get_image_from_s3(bucket = S3_BUCKET, folder = S3_UTIL_FOLDER, file = config["mask_image"])
+
+    # face detection
+    faces = face_detector(source_image) # = [[x0, y0, w0, h0], ... ]
+    if len(faces) > 0:
+        face_image = trim_image(source_image, faces[0])
+    else:
+        return { "error": "cannot find faces" }
+
+    # merge images
+    sized_face_image  = cv2.resize(face_image,(mask_image.shape[0],mask_image.shape[1]),cv2.INTER_LINEAR)
+    cx, xy = config["center"]
+    center = (cx, xy)
+    output_image    = cv2.seamlessClone(sized_face_image, base_image, mask_image, center, cv2.NORMAL_CLONE)
+
+    # upload image to S3
+    generated_image_filename = uuid.uuid4().hex + '.png';
+    put_image_to_s3(image = output_image, bucket = S3_BUCKET, folder = S3_OUTPUT_FOLDER, file = generated_image_filename)
+
+    image_url = 'https://s3-ap-northeast-1.amazonaws.com/' + S3_BUCKET + '/' + S3_OUTPUT_FOLDER + generated_image_filename;
+    return { "image_url": image_url }
